8f4d50b188a3fd0b39e8b2b8f5644407abe764da
Dmitry Tulupov <Dit1981@yandex.ru>
2017-06-16 14:40:49 +0300

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	  <meta charset="utf-8">
+	  <title>MiniCRM Admin</title>
+	</head>
+	<body>
+		<h1>MiniCRM Admin</h1>
+		<ul id="menu">
+			<li><a href="../index.php">На сайт</a></li>			
+		</ul>
+		<p>Управление задачами</p>
+		<ul class="menu">			
+			<li><a href="./tasks/add.php">Добавление задач</a></li>
+			<li><a href="./tasks/edit.php">Редактирование задач</a></li>
+			<li><a href="./tasks/delete.php">Удаление задач</a></li>
+		</ul>
+		<p>Управление пользователями</p>
+		<ul class="menu">			
+			<li><a href="./users/add.php">Добавление пользователей</a></li>
+			<li><a href="./users/edit.php">Редактирование пользователей</a></li>
+			<li><a href="./users/add.php">Удаление пользователей</a></li>
+		</ul>
+	</body>
+</html>
